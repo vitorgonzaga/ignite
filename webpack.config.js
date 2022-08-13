44c79cf60871f52c1680794090294f59b7238d68
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-08-12 23:04:50 -0300

    react refresh settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin =  require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -24,18 +25,32 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, 'public')
     },
+    hot: true,
   },
   plugins: [
+    /**
+     * Devido a utilização do "&&" faz-se necessário utilizar-se de um
+     * hack para evitar error quando retornar false que é utilizar o filter
+     * no array
+     */
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        },
       },
       {
         test: /\.scss$/,
